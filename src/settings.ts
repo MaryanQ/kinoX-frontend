94a12d9758437cc7d5262051cda83c35637bb63f
MaryanQ <maryansalad7@outlook.dk>
2024-03-25 02:03:32 +0100

    changes

--- src/settings.ts
+++ src/settings.ts
@@ -1,11 +1,10 @@
 const isProduction = import.meta.env.MODE === "production";
 
-// Define the API URL for development mode
-const DEV_API_URL = "https://kinocinema.azurewebsites.net";
-
-const URL = isProduction ? import.meta.env.VITE_PROD_API_BASE_URL : DEV_API_URL;
+const URL = isProduction
+  ? import.meta.env.VITE_PROD_API_BASE_URL
+  : import.meta.env.VITE_DEV_API_BASE_URL;
 
 const MODE = isProduction ? "Production" : "Development";
 console.info(MODE + " API URL: " + URL);
-console.info("ENV", import.meta.env);
+// console.info("ENV", import.meta.env);
 export const API_URL = URL;
